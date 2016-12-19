54524b9a500063ebefff51a41ad42dd4d4b4d47a
naits <naitsirhcednav2016@gmail.com>
2016-12-19 23:04:11 +0100

    Added RoutePlannerFragment

---
+++ app/src/main/java/com/example/naits/railly/RoutePlannerFragment.java
@@ -0,0 +1,20 @@
+package com.example.naits.railly;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RoutePlannerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_route_planner, container, false);
+        return view;
+    }
+}

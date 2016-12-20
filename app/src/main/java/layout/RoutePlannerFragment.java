d40200ea47dc8891d432a62622829ad615e88428
naits <naitsirhcednav2016@gmail.com>
2016-12-20 14:59:46 +0100

    Solved Error

--- app/src/main/java/layout/RoutePlannerFragment.java
+++ app/src/main/java/layout/RoutePlannerFragment.java
@@ -1,4 +1,4 @@
-package com.example.naits.railly;
+package layout;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.naits.railly.R;
 
 
 public class RoutePlannerFragment extends Fragment {

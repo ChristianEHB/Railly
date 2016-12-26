cb5d5ec2adf75f1ef2e4ebdc7a143dbe7697b135
Rubenvdabeele <rubenvdabeele@gmail.com>
2016-12-26 15:07:12 +0100

    RouteActivity expandabvle list laten gebruiken, functionaliteit is matig, moet opgeleukt. Eerste stappen in de goeie richting

--- app/src/main/java/com/example/naits/railly/activities/RoutePickerActivity.java
+++ app/src/main/java/com/example/naits/railly/activities/RoutePickerActivity.java
@@ -11,15 +11,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.naits.railly.DAO.ConnectionDAO;
 import com.example.naits.railly.DAO.StationDAO;
 import com.example.naits.railly.R;
+import com.example.naits.railly.adapters.ExpandableRouteListAdapter;
 import com.example.naits.railly.model.Connection;
 import com.example.naits.railly.model.Route;
-import com.example.naits.railly.adapters.RouteListAdapter;
 
 import org.json.JSONObject;
 
@@ -30,8 +31,8 @@ import com.example.naits.railly.util.HttpHandler;
 
 public class RoutePickerActivity extends AppCompatActivity {
 
-    private ListView lvRoute;
-    private RouteListAdapter adapter;
+    private ExpandableListView lvRoute;
+    private ExpandableRouteListAdapter adapter;
     private List<Route> routeList;
 
     protected ProgressDialog progDialog;
@@ -43,7 +44,7 @@ public class RoutePickerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
+        setContentView(R.layout.activity_route_expandable);
 
         //retrieve data from prev activity
         Bundle extras = getIntent().getExtras();
@@ -127,11 +128,13 @@ public class RoutePickerActivity extends AppCompatActivity {
         protected void onPostExecute(Void unused) {
             super.onPostExecute(unused);
             progDialog.dismiss();
-            lvRoute = (ListView) findViewById(R.id.listView_routes);
 
-            routeList = new ArrayList<>();
+            lvRoute = (ExpandableListView) findViewById(R.id.expList_Routes);
 
-            adapter = new RouteListAdapter(getApplicationContext(), routeList);
+
+
+            //adapter = new RouteListAdapter(getApplicationContext(), routeList);
+            adapter = new ExpandableRouteListAdapter(getApplicationContext(),arrCon);
             lvRoute.setAdapter(adapter);
 
             lvRoute.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -147,7 +150,7 @@ public class RoutePickerActivity extends AppCompatActivity {
                     for (Connection c : arrCon) {
                         int i = 1;
                         Log.d("routeactivity:", c.toString());
-                        routeList.add(new Route(i, c.getDeparture().toString(), c.getArrival().toString()));
+                        //routeList.add(new Route(i, c.getDeparture().toString(), c.getArrival().toString()));
                         i++;
                     }
                 }
@@ -168,5 +171,89 @@ public class RoutePickerActivity extends AppCompatActivity {
 
 
     }
+
+
+
+//    private class AsyncRouteFetch extends AsyncTask<String, Integer, Void> {
+//
+//        ArrayList<Connection> arrCon = null;
+//
+//        public AsyncRouteFetch() {
+//
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            progDialog = new ProgressDialog(RoutePickerActivity.this);
+//            progDialog.setMessage("Loading...");
+//            progDialog.setIndeterminate(false);
+//            progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//            progDialog.setCancelable(true);
+//            progDialog.show();
+//        }
+//
+//        @Override
+//        protected Void doInBackground(String... param) {
+//            ConnectionDAO conDAO = new ConnectionDAO();
+//
+//            try {
+//                JSONObject JOstatCache = new HttpHandler().getJSONObjectFromStream(getResources().openRawResource(R.raw.stationcache));
+//                new StationDAO().loadCache(JOstatCache);
+//
+//                arrCon = (ArrayList<Connection>) conDAO.getConnections(param[0]);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//            return null;
+//
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void unused) {
+//            super.onPostExecute(unused);
+//            progDialog.dismiss();
+//            lvRoute = (ListView) findViewById(R.id.listView_routes);
+//
+//            routeList = new ArrayList<>();
+//
+//            adapter = new RouteListAdapter(getApplicationContext(), routeList);
+//            lvRoute.setAdapter(adapter);
+//
+//            lvRoute.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    Toast.makeText(getApplicationContext(), "Clicked on route = " + view.getTag(), Toast.LENGTH_LONG).show();
+//                }
+//            });
+//
+//
+//            if (arrCon != null) {
+//                if(arrCon.size() !=0) {
+//                    for (Connection c : arrCon) {
+//                        int i = 1;
+//                        Log.d("routeactivity:", c.toString());
+//                        routeList.add(new Route(i, c.getDeparture().toString(), c.getArrival().toString()));
+//                        i++;
+//                    }
+//                }
+//                else{
+//                    AlertDialog alertDialog = new AlertDialog.Builder(RoutePickerActivity.this).create();
+//                    alertDialog.setTitle("Sorry!");
+//                    alertDialog.setMessage("No routes could be found.");
+//                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+//                            new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    dialog.dismiss();
+//                                }
+//                            });
+//                    alertDialog.show();
+//                }
+//            }
+//        }
+//
+//
+//    }
 
 }
---
+++ app/src/main/java/com/example/naits/railly/adapters/ExpandableRouteListAdapter.java
@@ -0,0 +1,121 @@
+package com.example.naits.railly.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.naits.railly.R;
+import com.example.naits.railly.model.Connection;
+import com.example.naits.railly.util.DateUtil;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by Ruben on 26/12/2016.
+ */
+
+public class ExpandableRouteListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<Connection> connections;
+
+    public ExpandableRouteListAdapter(Context context, List<Connection> connections) {
+        this.context = context;
+        this.connections = connections;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return connections.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        //connections[i] = connection -> connection.getVias = vias -> vias.size();
+        return connections.get(groupPosition).getVias().size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return connections.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return connections.get(groupPosition).getVias().get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return connections.get(groupPosition).getId();
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return connections.get(groupPosition).getVias().get(childPosition).getId();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        // ???
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        View v = View.inflate(context, R.layout.item_route_list, null);
+        TextView departureTextView = (TextView) v.findViewById(R.id.route_item_departure);
+        TextView arrivalTextView = (TextView) v.findViewById(R.id.route_item_arrival);
+        TextView arrivalTimeTextView = (TextView) v.findViewById(R.id.route_item_arrivalTime);
+        TextView departureTimeTextView = (TextView) v.findViewById(R.id.route_item_departureTime);
+
+        String departureText, arrivalText, departureTimeText, arrivalTimetext;
+
+        departureText = connections.get(groupPosition).getDeparture().getToStation().getName();
+        arrivalText = connections.get(groupPosition).getArrival().getToStation().getName();
+
+        departureTimeText = DateUtil.timeStampToDate(connections.get(groupPosition).getDeparture().getDepartureInfo().getTimeStamp());
+        arrivalTimetext = DateUtil.timeStampToDate(connections.get(groupPosition).getArrival().getDepartureInfo().getTimeStamp());
+
+        departureTextView.setText(departureText);
+        arrivalTextView.setText(arrivalText);
+
+        departureTimeTextView.setText(departureTimeText);
+        arrivalTimeTextView.setText(arrivalTimetext);
+
+        return v;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        View v = View.inflate(context, R.layout.item_route_list, null);
+        TextView departureTextView = (TextView) v.findViewById(R.id.route_item_departure);
+        TextView arrivalTextView = (TextView) v.findViewById(R.id.route_item_arrival);
+        TextView arrivalTimeTextView = (TextView) v.findViewById(R.id.route_item_arrivalTime);
+        TextView departureTimeTextView = (TextView) v.findViewById(R.id.route_item_departureTime);
+
+        String departureText, arrivalText, departureTimeText, arrivalTimetext;
+
+        departureText = connections.get(groupPosition).getVias().get(childPosition).getStation().getName();
+        arrivalText = connections.get(groupPosition).getVias().get(childPosition).getDepInfo().toString();
+
+        //departureTimeText = DateUtil.timeStampToDate(connections.get(groupPosition).getVias().get(childPosition).getDepInfo().toString());
+        //arrivalTimetext = DateUtil.timeStampToDate(connections.get(groupPosition).getArrival().getDepartureInfo().getTimeStamp());
+
+        departureTextView.setText(departureText);
+        arrivalTextView.setText(arrivalText);
+
+        //departureTimeTextView.setText(departureTimeText);
+        //arrivalTimeTextView.setText(arrivalTimetext);
+
+        return v;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
